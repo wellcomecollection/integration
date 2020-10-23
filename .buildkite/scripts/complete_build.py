e6e9691491ec7652cca3ab4d57b460545e35fb11
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-10-23 17:02:52 +0100

    Wip

--- .buildkite/scripts/complete_build.py
+++ .buildkite/scripts/complete_build.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8
 
-from commands import git
+# from commands import git
 from provider import current_branch, is_default_branch
 
 
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     branch_name = current_branch()
 
     if is_default_branch():
-        print(f"Successful completion on {branch_name}, tagging latest.")
+        # print(f"Successful completion on {branch_name}, tagging latest.")
 
-        git("tag", "latest", "--force")
-        git("push", "origin", "latest", "--force")
+        # git("tag", "latest", "--force")
+        # git("push", "origin", "latest", "--force")
 
         print("Done.")
     else:
